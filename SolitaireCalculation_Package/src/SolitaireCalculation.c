03d8fb5a99b2838d28bb091a34ba750e449c5bbf
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2024-11-08 20:10:21 +0900

    define options

--- SolitaireCalculation_Package/src/SolitaireCalculation.c
+++ SolitaireCalculation_Package/src/SolitaireCalculation.c
@@ -1,20 +1,101 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
+#include <string.h> // strcmp
+#include <stdlib.h> // exit, random
+#include <time.h>   // time
 
-//                                        turn_  1
-//              .------_:=^=:_------_:=^=:_------_:=^=:_------_:=^=:_------.
-// point:              |  1  |      |  2  |      |  3  |      |  4  |
-//     lead:     .-----.      .-----.      .-----.      .-----.
-// $ C_ K        |   2 | S_ A |   4 | D_ 2 |   6 | H_ 3 |   8 | C_ 4
-//               '-----'      '-----'      '-----'      '-----'
-//     stuck:
+#define OPTION_HELP {"-h", "help", "HELP"}        // info of options
+#define OPTION_TREE {"-t", "tree", "TREE"}        // show struct of path in data
+#define OPTION_DIRECTOTY {"-d", "dir", "DIR"}     // show file or subdirectory in data
+#define OPTION_REMOVE {"-rm", "remove", "REMOVE"} // remove directory, file in data
+#define OPTION_ADD {"-a", "add", "ADD"}           // create directory, file in data
 
-// ms (1~4):makestack stuckpoint
-// dc (1~4):discard leadpoint
-// ds (1~4) (1~4):disstuck stuckpoint leadpoint
-// ud :undo
-// ed :endgame
+// 'S', 'D', 'H', 'C'
+int ranks[] = {'S', 'D', 'H', 'C'};
+// 'A', '2', '3', '4', '5', '6', '7', '8', '9', "10", 'J', 'Q', 'K'
+int suits[] = {'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'};
+typedef struct
+{
+    char *suit; // 'S', 'D', 'H', 'C'
+    char *rank; // 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'
+} Card;
+typedef struct
+{
+    Card *next;
+} Stock;
 
-// ed
+// To Enter Game
+int StartWindow(int, char **);
+void cmdline_error();
+
+// Enter Game
+void SolitaireCalculation();
+
+/*
+
+                                       turn_  1
+             .------_:=^=:_------_:=^=:_------_:=^=:_------_:=^=:_------.
+point:              |  1  |      |  2  |      |  3  |      |  4  |
+    lead:     .-----.      .-----.      .-----.      .-----.
+$ C_ K        |   2 | S_ A |   4 | D_ 2 |   6 | H_ 3 |   8 | C_ 4
+              '-----'      '-----'      '-----'      '-----'
+    stuck:
+
+ms (1~4):makestack stuckpoint
+dc (1~4):discard leadpoint
+ds (1~4) (1~4):disstuck stuckpoint leadpoint
+ud :undo
+ed :endgame
+
+ed
+
+    puts("\x1b[?25l\x1b[H\x1b[J"); // hide cursor, move it to HOME and delete right of cousor pos
+
+
+    puts("\x1b[?25h"); // show cursor
+
+*/
+
+int main(int argc, char **argv)
+{
+    StartWindow(argc, argv);
+    SolitaireCalculation();
+
+    return 0;
+}
+
+void SolitaireCalculation()
+{
+    puts("SolitaireCalculation starts");
+}
+
+int StartWindow(int argc, char **argv)
+{
+    char *options[] = {OPTION_ADD, OPTION_DIRECTOTY, OPTION_HELP, OPTION_REMOVE, OPTION_TREE};
+    if (argc == 1)
+    {
+        // Normal Startment
+        return 0;
+    }
+    else if (argc == 2)
+    {
+        // Option Startment
+        if (argv[1])
+        {
+        }
+    }
+    else if (argc == 3)
+    {
+    }
+    else
+    {
+        cmdline_error();
+    }
+
+    return 0;
+}
+
+void cmdline_error()
+{
+    puts("arguments are too much\nUsage: .\\SolitaireCalculation.exe <option>");
+    exit(EXIT_FAILURE);
+}
