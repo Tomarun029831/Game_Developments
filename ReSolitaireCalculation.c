bc9f2c0b99363e394548a9b58ff07d8a11c89d86
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2024-11-08 17:50:23 +0900

    project starts

---
+++ ReSolitaireCalculation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//                                        turn_  1
+//              .------_:=^=:_------_:=^=:_------_:=^=:_------_:=^=:_------.
+// point:              |  1  |      |  2  |      |  3  |      |  4  |
+//     lead:     .-----.      .-----.      .-----.      .-----.
+// $ C_ K        |   2 | S_ A |   4 | D_ 2 |   6 | H_ 3 |   8 | C_ 4
+//               '-----'      '-----'      '-----'      '-----'
+//     stuck:
+
+// ms (1~4):makestack stuckpoint
+// dc (1~4):discard leadpoint
+// ds (1~4) (1~4):disstuck stuckpoint leadpoint
+// ud :undo
+// ed :endgame
+
+// ed
