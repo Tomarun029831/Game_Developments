d197b7619df30c2ffe7117b014858043d0cc9b32
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2024-11-08 18:01:26 +0900

    init path, decide struct of file

--- SolitaireCalculation_Package/src/ReSolitaireCalculation.c
+++ SolitaireCalculation_Package/src/ReSolitaireCalculation.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 //                                        turn_  1
 //              .------_:=^=:_------_:=^=:_------_:=^=:_------_:=^=:_------.
